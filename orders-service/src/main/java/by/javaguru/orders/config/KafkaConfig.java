cc2f672cf6ac6bc4190c17bab20f6b2fabb20ec0
andrey <tanchak12@gmail.com>
2025-11-05 23:06:31 +0200

    launched kafka cluster and first http postman requests

--- orders-service/src/main/java/by/javaguru/orders/config/KafkaConfig.java
+++ orders-service/src/main/java/by/javaguru/orders/config/KafkaConfig.java
@@ -1,16 +1,36 @@
 package by.javaguru.orders.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaConfig {
 
+    private static final Integer TOPIC_REPLICATION_FACTOR = 3;
+    private static final Integer TOPIC_PARTITIONS = 3;
+
+    @Value("orders.events.topic.name")
+    private String orderEventsTopicName;
+
+
     @Bean
     KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
     }
+
+    @Bean
+    NewTopic orderEventsTopic() {
+        return TopicBuilder.name(orderEventsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
+
+
 
 }
--- orders-service/src/main/java/by/javaguru/orders/service/OrderServiceImpl.java
+++ orders-service/src/main/java/by/javaguru/orders/service/OrderServiceImpl.java
@@ -1,17 +1,26 @@
 package by.javaguru.orders.service;
 
 import by.javaguru.core.dto.Order;
+import by.javaguru.core.dto.OrderCreatedEvent;
 import by.javaguru.core.types.OrderStatus;
 import by.javaguru.orders.dao.jpa.entity.OrderEntity;
 import by.javaguru.orders.dao.jpa.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final String orderEventsTopicName;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository,
+                            KafkaTemplate<String, Object> kafkaTemplate,
+                            @Value("orders.events.topic.name") String orderEventsTopicName) {
         this.orderRepository = orderRepository;
+        this.kafkaTemplate = kafkaTemplate;
+        this.orderEventsTopicName = orderEventsTopicName;
     }
 
     @Override
@@ -22,6 +31,15 @@ public class OrderServiceImpl implements OrderService {
         entity.setProductQuantity(order.getProductQuantity());
         entity.setStatus(OrderStatus.CREATED);
         orderRepository.save(entity);
+
+        OrderCreatedEvent placeOrder = new OrderCreatedEvent(
+                entity.getId(),
+                entity.getCustomerId(),
+                entity.getProductId(),
+                entity.getProductQuantity()
+        );
+
+        kafkaTemplate.send(orderEventsTopicName, placeOrder);
 
         return new Order(
                 entity.getId(),
